07a0856dc2519621091d91a6aa0fb26fe8ff4945
Илья Тарасов <ilyatarasov2006@gmail.com>
2025-04-18 14:21:36 +0300

    Created method remove delete folder on cloud and add docs for all method

--- handlers.py
+++ handlers.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import json
 from dotenv import load_dotenv
 from datetime import datetime, timezone
 
@@ -13,36 +12,57 @@ INTERVAL_SYNCHRONISATION_MINUTES = int(os.getenv("INTERVAL_SYNCHRONISATION_MINUT
 
 
 def check_folder_and_execution_of_works(dir_skan, cloud):
+    """Проверяет нахождение новых файлов в указанной директории и создает копии в облачном хранилище или удаляет лишние файлы в облачном хранилище."""
+    if not os.path.exists(dir_skan):
+        return
     subdir = os.path.relpath(dir_skan, DIR_SKAN) if dir_skan != DIR_SKAN else None
 
-    # Получаем списки файлов и папок
     all_entries = os.listdir(dir_skan)
     dirs_only = [f for f in all_entries if os.path.isdir(os.path.join(dir_skan, f))]
     files_only = [f for f in all_entries if os.path.isfile(os.path.join(dir_skan, f))]
 
-    # Формируем информацию о локальных файлах (храним timestamp)
     os_files_info = [
         {
             'name': f,
-            'updated': os.path.getmtime(os.path.join(dir_skan, f))  # Сохраняем timestamp
+            'updated': os.path.getmtime(os.path.join(dir_skan, f))
         }
         for f in files_only
         if '.' in f and not f.startswith('.')
     ]
 
-    # Получаем данные из облака
-    cloud_files = cloud.get_in_dir(subdir) or []
+    cloud_response = cloud.get_in_dir(subdir) or []
+
     cloud_files = [
-        f for f in cloud_files
+        f for f in cloud_response
         if '.' in f['name'] and not f['name'].startswith('.')
     ]
+    cloud_folders = [
+        d for d in cloud_response
+        if '.' not in d['name'] and not d['name'].startswith('.')
+    ]
+    cloud_folders_set = set()
 
-    # Создаем словари для быстрого поиска
+    dirs_only_set = set()
+
+    for dir_only in dirs_only:
+        dirs_only_set.add(dir_only)
+
+    for cloud_file in cloud_folders:
+        cloud_folders_set.add(cloud_file['name'])
+
+    folder_only = dirs_only_set != cloud_folders_set
+    if folder_only:
+        folders_cloud_delete = [folder
+                              for folder in cloud_folders_set - dirs_only_set
+                              if len(cloud_folders_set) > len(dirs_only_set)
+                              ]
+        for folder in folders_cloud_delete:
+            cloud.delete_folder_iterative(subdir + '/' + folder if subdir is not None else '' + folder)
+
     local_files_map = {f['name']: f['updated'] for f in os_files_info}  # timestamp
     cloud_files_map = {f['name']: datetime.fromisoformat(f['updated']).timestamp()
                        for f in cloud_files}  # Конвертируем в timestamp
 
-    # Находим расхождения
     common_files = set(local_files_map) & set(cloud_files_map)
     time_diffs = [
         {
@@ -57,7 +77,6 @@ def check_folder_and_execution_of_works(dir_skan, cloud):
         if abs(local_files_map[name] - cloud_files_map[name]) > INTERVAL_SYNCHRONISATION_MINUTES * 60 and abs(local_files_map[name] - cloud_files_map[name]) < INTERVAL_SYNCHRONISATION_MINUTES * 60 * 3
     ]
 
-    print(f"Local only: {set(local_files_map) - set(cloud_files_map)}")
     local_only = set(local_files_map) - set(cloud_files_map)
     if local_only:
         for file_name in local_only:
@@ -66,7 +85,6 @@ def check_folder_and_execution_of_works(dir_skan, cloud):
             if upload:
                 print(f"Файл {file_name} успешно загружен в облако")
 
-    print(f"Cloud only: {set(cloud_files_map) - set(local_files_map)}")
     cloud_only = set(cloud_files_map) - set(local_files_map)
     if cloud_only:
         for file_name in cloud_only:
@@ -74,7 +92,7 @@ def check_folder_and_execution_of_works(dir_skan, cloud):
             delete = cloud.delete_file_from_cloud(file_name, subdir)
             if delete:
                 print(f"Файл {file_name} успешно удален из облака")
-    print(f"Time diffs: {time_diffs}")
+
     if time_diffs:
         for file_name in time_diffs:
             print(f"Обновление файла {file_name['name']} в облако из локальной папки {dir_skan}")
@@ -84,16 +102,19 @@ def check_folder_and_execution_of_works(dir_skan, cloud):
 
 
     for dir_name in dirs_only:
-        check_folder(os.path.join(dir_skan, dir_name), cloud)
+        check_folder_and_execution_of_works(os.path.join(dir_skan, dir_name), cloud)
 
 
 class CloudController:
     def __init__(self, api_key, path):
+        """Инициализирует объект класса CloudController с ключом API и путем к папке."""
         self.url = "https://cloud-api.yandex.net/v1/disk/resources"
         self.headers = {'Authorization': f'OAuth {api_key}'}
         self.path = path
 
+
     def get_in_dir(self, subdir=None):
+        """Получает список содержимого папки в облаке по заданному пути."""
         params = {
             "path": f"/{self.path}",
             "limit": 1000,
@@ -110,7 +131,9 @@ class CloudController:
                 response.append({'name': item['name'], 'updated': item['modified']})
             return response
 
+
     def upload_to_cloud(self, file_path, filename, subdir=None):
+        """Загружает файл в облако по указанному пути и имени файла."""
         url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         params = {
             "path": f"/{self.path}/{filename}",
@@ -142,7 +165,9 @@ class CloudController:
 
         return True
 
+
     def delete_file_from_cloud(self, filename, subdir=None):
+        """Удаляет файл из облака по указанному пути и имени файла."""
         params = {
             "path": f"/{self.path}/{filename}",
             "permanently": True
@@ -153,11 +178,14 @@ class CloudController:
         response = requests.delete(self.url, headers=self.headers, params=params)
 
         if response.status_code == 204:
+            print(f"Файл {filename} успешно удален из облака")
             return True
         else:
-            print(f"Ошибка при удалении файла: {filename}")
+            print(f"Ошибка при удалении файла: {params['path']}")
+
 
     def create_folder_in_cloud(self, subdir=None):
+        """Создает папку в облаке по указанному пути."""
         params = {
             "path": f"/{self.path}/{subdir}",
         }
@@ -170,3 +198,65 @@ class CloudController:
             print(f"Ошибка при создании папки: {subdir}")
         else:
             return True
+
+
+    def get_folder_contents(self, folder_path):
+        """Получает список содержимого папки"""
+
+        params = {
+            "path": f'{self.path}/{folder_path}',
+            "limit": 1000,
+            "sort": "name"
+        }
+
+        try:
+            response = requests.get(self.url, headers=self.headers, params=params)
+            if response.status_code == 200:
+                return response.json().get('_embedded', {}).get('items', [])
+            return []
+        except requests.exceptions.RequestException:
+            return []
+
+
+    def delete_single_folder(self, folder_path):
+        """Удаляет пустую папку"""
+        params = {"path": f'{self.path}/{folder_path}', "permanently": "true"}
+
+        response = requests.delete(self.url, headers=self.headers, params=params)
+        if response.status_code == 204:
+            print(f"Папка {params['path']} успешно удалена")
+            return True
+        else:
+            print(f"Ошибка при удалении папки: {params['path']}")
+
+
+    def delete_folder_iterative(self, folder_path):
+        """Итеративное удаление с использованием стека"""
+        stack = [folder_path]
+        deleted_items = set()
+
+
+        while stack:
+            current_path = stack.pop()
+
+            # Получаем содержимое текущей папки
+            contents = self.get_folder_contents(current_path)
+
+
+            if not contents:
+                # Если папка пуста - удаляем её
+                if self.delete_single_folder(current_path):
+                    deleted_items.add(current_path)
+                continue
+
+            # Добавляем текущую папку обратно в стек
+            stack.append(current_path)
+
+            # Добавляем все элементы папки в стек
+            for item in contents:
+                if item['path'] not in deleted_items:
+                    path_list = ['/'+i for i in item['path'].split('/')[2:]]
+                    path = ''.join(path_list)
+                    self.delete_file_from_cloud(path[1:])
+
+        return True
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ DIR_SKAN = os.getenv("DIR_SKAN")
 DISK_DIR = os.getenv("DISK_DIR")
 
 cloud = CloudController(API_KEY, DISK_DIR)
+
 while True:
     check_folder_and_execution_of_works(DIR_SKAN, cloud)
     sleep(INTERVAL_SYNCHRONISATION_MINUTES * 60)
